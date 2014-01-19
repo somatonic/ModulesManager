7d88b17b8f1e9c608628e5750e3788e1b9524d6d
somatonic <philipp@urlich.ch>
2014-01-19 21:14:19 +0100

    major update to 2.0.0 using jQuery DataTables
    requires now JqueryDataTables ProcessWire module

--- ModulesManager.js
+++ ModulesManager.js
@@ -1,174 +1,147 @@
+;
+(function($, config) {
+
+    var mmcat = config.process_modulesmanager_filter_cat ? "?cat=" + config.process_modulesmanager_filter_cat : '';
 
 
-$.fn.MMliveUpdate = function(list) {
+    $(function() {
 
-    list = $(list);
+        var aSelected = [];
 
-    if ( list.length ) {
+        var settings = {
+            "bStateSave": true,
+            "bProcessing": true,
+            "sPaginationType": "full_numbers",
+            "bSort": true,
+            "aaSorting": [
+                [4, 'desc']
+                // [1, 'asc']
+            ],
+            "sAjaxSource": config.process_modulesmanager + mmcat,
+            "fnRowCallBack": function(nRow, aData, iDisplayIndex) {
+                if (jQuery.inArray(aData.DT_RowId, aSelected) !== -1) {
+                    $(nRow).addClass('row_selected');
+                }
+            }
+        };
 
-        var rows = list.find('tr');
-        var cache = rows.map(function() {
-            var bit = $(this).children('td:first').text().toLowerCase();
-            bit += " " + $(this).children('td:eq(2)').text().toLowerCase();
-            bit += " " + $(this).children('td:eq(3)').text().toLowerCase();
-            bit += " " + $(this).children('td:eq(8)').text().toLowerCase();
-            return bit; //.replace("->"," ")
+        var $datatable = $("#mm_datatable").dataTable(settings);
+
+
+        /* Click event handler */
+        $('#mm_datatable').on('click', 'tbody tr', function() {
+            var id = this.id;
+            var index = jQuery.inArray(id, aSelected);
+
+            if (index === -1) {
+                aSelected.push(id);
+            } else {
+                aSelected.splice(index, 1);
+            }
+
+            $(this).toggleClass('row_selected');
         });
 
-        console.log(cache);
-        resetTable();
 
-        this
-            .keyup(filter).keyup()
-            .parents('form').submit(function() {
-            //return false;
-            });
-    }
+        var is_more_link = false;
 
-    return this;
+        $('#mm_datatable').on('click', 'tbody tr a', function(e) {
 
-    function filter() {
-        var term = $.trim( $(this).val().toLowerCase().replace("->"," ") ), scores = [];
-        if ( !term ) {
+            e.preventDefault();
 
-            resetTable();
+            is_more_link = $(this).hasClass("more_link");
 
-        } else {
+            if ($(this).hasClass("confirm")) {
+                var ok = confirm($(this).data("confirmtext"));
+                if (ok) {
 
-            rows.addClass('mm_notfiltered');
+                    if (config.process_modulesmanager_lightbox == "fancybox") {
+                        $(this).addClass("iframe");
+                        initFancybox($(this));
+                    }
 
-            cache.each(function(i){
-                var score = this.score(term);
-                if (score > 0.7) { scores.push([score, i]); }
-            });
+                    if (config.process_modulesmanager_lightbox == "magnific") {
+                        $.magnificPopup.open({
+                            items: {
+                                src: $(this).attr("href")
+                            },
+                            type: 'iframe',
+                            disableOn: 0,
+                            callbacks: {
+                                afterClose: function() {
+                                    if (!is_more_link) reloadDataTable($datatable);
+                                }
+                            }
+                        });
+                    }
 
-            $.each( scores.sort( function(a, b){ return b[0] - a[0]; }), function() {
-                $(rows[ this[1] ]).removeClass('mm_notfiltered');
-            });
+                }
+            } else {
 
-        }
-    }
-};
+                if (config.process_modulesmanager_lightbox == "fancybox") {
+                    $(this).addClass("iframe");
+                    initFancybox($(this));
+                }
+
+                if (config.process_modulesmanager_lightbox == "magnific") {
+                    $.magnificPopup.open({
+                        items: {
+                            src: $(this).attr("href")
+                        },
+                        type: 'iframe',
+                        disableOn: 0,
+                        callbacks: {
+                            afterClose: function() {
+                                if (!is_more_link) reloadDataTable($datatable);
+                            }
+                        }
+                    });
+                }
+            }
 
 
-$(function(){
+        });
 
-    //$('#filter').liveUpdate('table');
-    $('#mm_filter').live('click *',function(e){
-      //console.log($(this).val());
-      if($(this).val().length < 1){
-        resetTable();
-        $(this).keyup();
-      }
+        var initFancybox = function($link) {
+
+            var h = $(window).height() - 65;
+            var w = $(window).width() > 1150 ? 1150 : $(window).width() - 100;
+            $link.fancybox({
+                hideOnContentClick: true,
+                centerOnScroll: true,
+                frameWidth: w,
+                frameHeight: h,
+                callbackOnStart: function() {
+                    $('body').css('overflow', 'hidden');
+                },
+                callbackOnClose: function() {
+                    $('body').css('overflow', 'auto');
+                    if (!is_more_link) reloadDataTable($datatable);
+                }
+            }).trigger('click');
+        };
+
     });
 
+    var reloadDataTable = function($datatable) {
+        $.ajax({
+            url: config.process_modulesmanager + mmcat,
+            dataType: 'json',
+            type: 'post',
+            beforeSend: function() {
+                $('#mm_datatable_processing').css('visibility', 'visible');
+                $datatable.fadeTo("fast", 0.5);
+            },
+            success: function(data) {
+                console.log(data);
+                $datatable.fnClearTable();
+                $datatable.fnAddData(data.aaData);
+                $datatable.fadeTo("fast", 1);
+                $('#mm_datatable_processing').css('visibility', 'hidden');
 
-    // init search listener
-    $('input#mm_filter').MMliveUpdate('#modules_form tbody');
-
-});
-
-
-var resetTable = function(){
-    $('#modules_form tbody tr.mm_notfiltered').removeClass("mm_notfiltered");
-}
-
-
-
-
-// qs_score - Quicksilver Score
-//
-// A port of the Quicksilver string ranking algorithm
-//
-// "hello world".score("axl") //=> 0.0
-// "hello world".score("ow") //=> 0.6
-// "hello world".score("hello world") //=> 1.0
-//
-// Tested in Firefox 2 and Safari 3
-//
-// The Quicksilver code is available here
-// http://code.google.com/p/blacktree-alchemy/
-// http://blacktree-alchemy.googlecode.com/svn/trunk/Crucible/Code/NSString+BLTRRanking.m
-//
-// The MIT License
-//
-// Copyright (c) 2008 Lachie Cox
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+            }
+        });
+    };
 
 
-String.prototype.score = function(abbreviation,offset) {
-  offset = offset || 0 // TODO: I think this is unused... remove
-
-  if(abbreviation.length == 0) return 0.9
-  if(abbreviation.length > this.length) return 0.0
-
-  for (var i = abbreviation.length; i > 0; i--) {
-    var sub_abbreviation = abbreviation.substring(0,i)
-    var index = this.indexOf(sub_abbreviation)
-
-
-    if(index < 0) continue;
-    if(index + abbreviation.length > this.length + offset) continue;
-
-    var next_string       = this.substring(index+sub_abbreviation.length)
-    var next_abbreviation = null
-
-    if(i >= abbreviation.length)
-      next_abbreviation = ''
-    else
-      next_abbreviation = abbreviation.substring(i)
-
-    var remaining_score   = next_string.score(next_abbreviation,offset+index)
-
-    if (remaining_score > 0) {
-      var score = this.length-next_string.length;
-
-      if(index != 0) {
-        var j = 0;
-
-        var c = this.charCodeAt(index-1)
-        if(c==32 || c == 9) {
-          for(var j=(index-2); j >= 0; j--) {
-            c = this.charCodeAt(j)
-            score -= ((c == 32 || c == 9) ? 1 : 0.15)
-          }
-
-          // XXX maybe not port this heuristic
-          //
-          //          } else if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:matchedRange.location]]) {
-          //            for (j = matchedRange.location-1; j >= (int) searchRange.location; j--) {
-          //              if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:j]])
-          //                score--;
-          //              else
-          //                score -= 0.15;
-          //            }
-        } else {
-          score -= index
-        }
-      }
-
-      score += remaining_score * next_string.length
-      score /= this.length;
-      return score
-    }
-  }
-  return 0.0
-}
-
+})(jQuery, config);
